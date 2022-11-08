6ef377c818b565f07d70da540b657b80ed47b862
kiennguyenGH <ktnguyen4647@gmail.com>
2022-11-08 14:00:39 -0800

    Minor text change

--- src/User.java
+++ src/User.java
@@ -1,12 +1,25 @@
+import java.util.ArrayList;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 public class User extends DefaultMutableTreeNode implements CompositeUser{
     
     private String ID;
+    private ArrayList<User> followers;
     
     public User(String ID)
     {
         this.ID = ID;
+        followers = new ArrayList<User>();
+    }
+    
+    public ArrayList<User> getFollowers()
+    {
+        return followers;
+    }
+    
+    public void follow(User user)
+    {
+        user.getFollowers().add(this);
     }
     
     @Override
